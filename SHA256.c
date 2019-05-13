f2bbcbcf242fd6a265b2df9e63bedf38e859105f
KillyBOT <edwards.kyle.j@gmail.com>
2019-05-13 13:02:56 -0400

    SHA256 v3
    I fixed a little bug. I had a 32 bit variable for the size, when it should've been 64 bit. Kind of stupid...

--- SHA256.c
+++ SHA256.c
@@ -44,7 +44,7 @@ int main(int argc, char** argv){
 
 	int continueReading = 1;
 	int paddingNext = 0;
-	int fileSize = 0;
+	int64_t fileSize = 0;
 	int fileSizeToAdd;
 	//Why these values? They're the first fractional part of the square root of the first 8 prime numbers, I think...
 	word hash[HASH_SIZE] = {0x6a09e667, 0xbb67ae85,0x3c6ef372,0xa54ff53a,0x510e527f,0x9b05688c,0x1f83d9ab,0x5be0cd19};
